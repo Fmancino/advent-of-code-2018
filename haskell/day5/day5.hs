d90f5e0d13da3b9258f3b5015c168e9d76704356
Fmancino <17271080+Fmancino@users.noreply.github.com>
2019-02-19 00:18:56 +0100

    Solved speed problem
    Haskell lists are simple linked lists, therefore last method is very slow.

--- haskell/day5/day5.hs
+++ haskell/day5/day5.hs
@@ -20,16 +20,16 @@ traverseRmDuplicate i = traverseRmDuplicateImpl [] (head i, head $ tail i) (tail
 traverseRmDuplicateImpl :: [Char] -> (Char, Char) -> [Char] -> [Char]
 traverseRmDuplicateImpl before comp [] =
           if isSameInverted comp
-              then before
-              else before ++ [fst comp, snd comp]
+              then reverse before
+              else (reverse before) ++ [fst comp, snd comp]
 traverseRmDuplicateImpl [] comp after =
           if isSameInverted comp
               then traverseRmDuplicateImpl [] (head after, head $ tail after) (tail $ tail after)
               else traverseRmDuplicateImpl [fst comp] (snd comp, head after) (tail after)
 traverseRmDuplicateImpl before comp after =
           if isSameInverted comp
-              then traverseRmDuplicateImpl (init before) (last before, head after) (tail after)
-              else traverseRmDuplicateImpl (before ++ [fst comp]) (snd comp, head after) (tail after)
+              then traverseRmDuplicateImpl (tail before) (head before, head after) (tail after)
+              else traverseRmDuplicateImpl ([fst comp] ++ before ) (snd comp, head after) (tail after)
 
 
 findShortest :: [Char] -> Int
